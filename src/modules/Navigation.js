c3cac1a3dd67b9c781c45856a34880d56c76ced8
Patrisio <45238380+Patrisio@users.noreply.github.com>
2020-06-05 23:45:24 +0300

    Подключил библиотеку для типографики

---
+++ src/modules/Navigation.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className='navigation'>
+        <ul>
+            <li>luih</li>
+            <li>34</li>
+            <li>kuhiyg</li>
+            <li>324</li>
+        </ul>
+      </div>
+    );
+  }
+} 
